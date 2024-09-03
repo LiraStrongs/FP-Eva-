df4a8d5f656e94bf6dea3dd3a20086ef393c5dfd
LiraStrongs <sebastianlira06@gmail.com>
2024-09-03 17:50:55 -0600

    Add files via upload

---
+++ EVA_10_operaciones_2/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+       //concatenacion
+        //si usamos el mismo simbolo con diferentes usos se llama SOBRECARGA de operaciones
+
+        String nombre="Sebastian Alain";
+        String apellido="Lira Zavala";
+        String nomcomp;
+        //concatenar
+        nomcomp= nombre+" "+apellido;
+        System.out.println(nomcomp);
+
+        System.out.println("nombre del usuario:" + nombre);
+        System.out.println("apellido del usuario:" + apellido);
+
+
+
+
+        }
+    }
---
+++ EVA_9_ operacion_variables/src/Main.java
@@ -0,0 +1,60 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Las operaciones dependen del tipo de datos
+
+        //Valores logicos (boolean)
+        //operaciones logicas --> true o false
+        //                       and, not, or
+        //                        Mate discretas --->Logica Booleana
+
+        //cadenas
+        //pegarlas (concatenar)
+        //1potencias y raices,2 multriplicacion y division y 3 sumas y restas
+
+        //para todas las operaciones se usan operadores (simobolos)
+        //OPERACIONES ARITMETICAS
+
+        int x=10; //declaracion y asignacion
+        int y=5;
+        int suma, resta, multriplicación, division,potencia, raiz;
+
+        suma= x+y;
+        resta= x-y;
+        multriplicación= x*y;
+        division= x/y;
+
+
+        System.out.println("la suma de X y Y tomando en cuenta que X=10 y Y=5 es....");
+        System.out.println(suma);
+        System.out.println("esta es la resta de X y Y tomando en cuenta que X=10 y Y=5 ");
+        System.out.println(resta);
+        System.out.println("esta es la multriplicacion tomando en cuenta que X=10 y Y=5 ");
+        System.out.println(multriplicación);
+        System.out.println("esta es la division tomando en cuenta que X=10 y Y=5 ");
+        System.out.println(division);
+
+        System.out.println("cambiamos los valores a X=5 y Y=2 y dvidimos");
+        x=5;
+        y=2;
+
+        double val1=5;
+        double val2=2;
+        double resu;
+        resu= val1/val2;
+
+        System.out.println(resu);
+
+
+
+
+
+
+        //suma y resta
+
+
+
+        }
+    }
